a2b3bd1c2800f040a1963d71e83e3b4082afaa7b
mixartemev <mixartemev@gmail.com>
2018-02-17 18:09:55 +0300

    models phpdoc

--- app/Acc.php
+++ app/Acc.php
@@ -2,13 +2,24 @@
 
 namespace Insta;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Acc
- * @package Insta
  *
+ * @package Insta
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $type
  * @property string $login
+ * @property string $pwd
+ * @property int $user_id
+ * @method static Builder|Acc whereId($value)
+ * @method static Builder|Acc whereLogin($value)
+ * @method static Builder|Acc wherePwd($value)
+ * @method static Builder|Acc whereType($value)
+ * @method static Builder|Acc whereUserId($value)
  */
 class Acc extends Model
 {
--- app/Order.php
+++ app/Order.php
@@ -2,8 +2,26 @@
 
 namespace Insta;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Insta\Order
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $type
+ * @property int $value
+ * @property int $acc_id
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static Builder|Order whereAccId($value)
+ * @method static Builder|Order whereCreatedAt($value)
+ * @method static Builder|Order whereId($value)
+ * @method static Builder|Order whereType($value)
+ * @method static Builder|Order whereUpdatedAt($value)
+ * @method static Builder|Order whereValue($value)
+ */
 class Order extends Model
 {
     //
