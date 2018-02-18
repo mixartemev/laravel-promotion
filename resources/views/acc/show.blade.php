709632d94102b746c56c6c42c1e1df15ab93cc55
mixartemev <mixartemev@gmail.com>
2018-02-18 23:11:34 +0300

    order post

--- resources/views/acc/show.blade.php
+++ resources/views/acc/show.blade.php
@@ -19,5 +19,22 @@
                 </div>
             </div>
         </div>
+    @if(auth()->check())
+        <hr>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="POST" action="{{$acc->path()}}/orders" class="form-inline">
+                    {{csrf_field()}}
+                    <select name="type" class="custom-select col-sm-4">
+                        @foreach(['like', 'coment', 'subscribe'] as $type)
+                            <option>{{$type}}</option>
+                        @endforeach
+                    </select>
+                    <input type="text" name="value" class="form-control col-sm-4 offset-sm-1"/>
+                    <button class="btn btn-default col-sm-2 offset-sm-1">Order</button>
+                </form>
+            </div>
+        </div>
+    @endif
     </div>
 @endsection
